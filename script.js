140a12388928733593309af1a9952c7830baf51d
xhvsh <82961902+xhvsh@users.noreply.github.com>
2023-12-03 15:15:43 +0100

    Update script.js (better performance)
    [03.12.2023] Updated JS for better performance.

--- script.js
+++ script.js
@@ -24,56 +24,36 @@ priceInput.addEventListener('input', () => {
 });
 
 btn.addEventListener('click', () => {
-  var type = document.querySelector('.type').value;
-  var participants = document.querySelector('.participants').value;
-  var price = document.querySelector('.price').value;
+  const type = document.querySelector('.type').value;
+  const participants = document.querySelector('.participants').value;
+  const price = document.querySelector('.price').value;
+  const url = type === 'random' ? `https://www.boredapi.com/api/activity?participants=${participants}&minprice=0.0&maxprice=${price}` : `https://www.boredapi.com/api/activity?type=${type}&participants=${participants}&minprice=0.0&maxprice=${price}`;
 
-  if (type == 'random') {
-    fetch(`https://www.boredapi.com/api/activity?participants=${participants}&minprice=0.0&maxprice=${price}`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.error) {
-          activityName.innerHTML = data.error;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) {
+        activityName.innerHTML = data.error;
+        [activityInfo, display, priceP].forEach((elem) => elem.classList.add('hidden'));
+      } else {
+        activityInfo.classList.remove('hidden');
+        display.classList.remove('hidden');
+        priceP.classList.remove('hidden');
 
-          activityInfo.classList.add('hidden');
-          display.classList.add('hidden');
-          priceP.classList.add('hidden');
-        } else {
-          activityInfo.classList.remove('hidden');
-          display.classList.remove('hidden');
-          priceP.classList.remove('hidden');
+        activityName.innerHTML = data.activity;
+        activityInfo.innerHTML = `Type: ${data.type} | Participants: ${data.participants}`;
+        activityPrice.value = data.price;
+        finalPrice.innerHTML = data.price;
 
-          activityName.innerHTML = data.activity;
-          activityInfo.innerHTML = `Type: ${data.type} | Participants: ${data.participants}`;
-          activityPrice.value = data.price;
-          finalPrice.innerHTML = data.price;
+        if (activityPrice.value === '0') {
+          console.log(activityPrice);
+          activityPrice.classList.add('free');
         }
-      })
-      .catch((err) => console.error(err));
-  } else {
-    fetch(`https://www.boredapi.com/api/activity?type=${type}&participants=${participants}&minprice=0.0&maxprice=${price}`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.error) {
-          activityName.innerHTML = data.error;
-
-          activityInfo.classList.add('hidden');
-          display.classList.add('hidden');
-          priceP.classList.add('hidden');
-        } else {
-          activityInfo.classList.remove('hidden');
-          display.classList.remove('hidden');
-          priceP.classList.remove('hidden');
-
-          activityName.innerHTML = data.activity;
-          activityInfo.innerHTML = `Type: ${data.type} | Participants: ${data.participants}`;
-          activityPrice.value = data.price;
-          finalPrice.innerHTML = data.price;
-        }
-      })
-      .catch((err) => console.error(err));
-  }
+      }
+    })
+    .catch((err) => console.error(err));
 });
+
 reset.addEventListener('click', () => {
   activityName.innerHTML = `Activity will be displayed there.`;
   activityInfo.innerHTML = ``;
